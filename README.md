91411c9d6f4c4b6438ccbf7ff1245ee12d5e6591
maliszew <petr@list.pl>
2017-01-25 15:57:24 +0100

    added bootstrap table; oauth2 enhancement part 1

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ Java Spring + MySQL + Maven + JQuery + Bootstrap + Gson + Mustache + OAuth + Pha
 
 ##### What is working
 RESTful API for products
+
 Listing all products in store, detailed description for every product on sepearate page
+
 Logging through OAuth (Facebook or Google account)
+
 Some tests in PhantomJS
 
 ##### What should work, but it's not
 Adding items to cart, generating payment
+
 User profile with orders
+
 Admin panel
--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -1,11 +1,16 @@
 package com.example.controllers;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
 
+import javax.inject.Inject;
 import java.util.Date;
 import java.util.Map;
 
@@ -13,11 +18,33 @@ import java.util.Map;
  * Created by Yoga2pro on 10/26/2016.
  */
 @Controller
+@RequestMapping("/")
 public class HomeController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String main(Map<String,Object> template) {
+//    private Facebook facebook;
+//     private ConnectionRepository connectionRepository;
+
+//    @SuppressWarnings("SpringJavaAutowiringInspection")
+//    @Inject
+//    public HomeController(Facebook facebook, ConnectionRepository connectionRepository) {
+//        this.facebook = facebook;
+//        this.connectionRepository = connectionRepository;
+//    }
+
+    // @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String mainPage(Map<String,Object> template, Model model) {
         Date date = new Date();
         template.put("time", date.toString());
+
+//        if (!facebook.isAuthorized()) {
+//            return "redirect:/connect/facebook";
+//        }
+
+//        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+//            return "redirect:/connect/facebook";
+//        }
+//
+//        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
         return "mainTemplate";
     }
 
